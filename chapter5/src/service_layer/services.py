67daf1447269d4f762b2b0c05d6be16e4cf3000f
babjo <ch.lee.3477@gmail.com>
2021-08-15 01:14:22 +0000

    Deploying to gh-pages from  @ 1b6bd3965e515aa2751ca3ae118e63eeefeb21c3 🚀

---
+++ chapter5/src/service_layer/services.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import Optional
+from datetime import date
+
+from ..domain import model
+from ..domain.model import OrderLine
+from ..adapters.repository import AbstractRepository
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(orderid: str, sku: str, qty: int, repo: AbstractRepository, session) -> str:
+    line = OrderLine(orderid, sku, qty)
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref
+
+
+def add_batch(ref: str, sku: str, qty: int, eta: Optional[date], repo, session):
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
